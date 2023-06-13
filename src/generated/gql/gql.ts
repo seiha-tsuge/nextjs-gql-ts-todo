29863cf16148b3d78de80907c5daf29f241492d7
seiha-tsuge <seiha.tsuge@gmail.com>
2023-06-13 13:51:22 +0900

    style: graphql-codegen Generate outputs

--- src/generated/gql/gql.ts
+++ src/generated/gql/gql.ts
@@ -13,10 +13,10 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-    "mutation MakeTodo($makeTodoInput: MakeTodoInput!) {\n  makeTodo(makeTodoInput: $makeTodoInput) {\n    todo {\n      id\n      title\n      isCompleted\n      createdAt\n      updatedAt\n    }\n  }\n}": types.MakeTodoDocument,
-    "mutation RemoveTodo($removeTodoInput: RemoveTodoInput!) {\n  removeTodo(removeTodoInput: $removeTodoInput) {\n    todo {\n      id\n      title\n    }\n  }\n}": types.RemoveTodoDocument,
-    "mutation UpdateTodo($updateTodoInput: UpdateTodoInput!) {\n  updateTodo(updateTodoInput: $updateTodoInput) {\n    todo {\n      id\n      title\n      isCompleted\n      updatedAt\n      createdAt\n    }\n  }\n}": types.UpdateTodoDocument,
-    "query Todos {\n  getTodos {\n    todos {\n      id\n      title\n      isCompleted\n      createdAt\n      updatedAt\n    }\n  }\n}": types.TodosDocument,
+    "\n  mutation MakeTodo($makeTodoInput: MakeTodoInput!) {\n    makeTodo(makeTodoInput: $makeTodoInput) {\n      todo {\n        id\n        title\n        isCompleted\n        createdAt\n        updatedAt\n      }\n    }\n  }\n": types.MakeTodoDocument,
+    "\n  mutation RemoveTodo($removeTodoInput: RemoveTodoInput!) {\n    removeTodo(removeTodoInput: $removeTodoInput) {\n      todo {\n        id\n        title\n      }\n    }\n  }\n": types.RemoveTodoDocument,
+    "\n  mutation UpdateTodo($updateTodoInput: UpdateTodoInput!) {\n    updateTodo(updateTodoInput: $updateTodoInput) {\n      todo {\n        id\n        title\n        isCompleted\n        updatedAt\n        createdAt\n      }\n    }\n  }\n": types.UpdateTodoDocument,
+    "\n  query Todos {\n    getTodos {\n      todos {\n        id\n        title\n        isCompleted\n        createdAt\n        updatedAt\n      }\n    }\n  }\n": types.TodosDocument,
 };
 
 /**
@@ -36,19 +36,19 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "mutation MakeTodo($makeTodoInput: MakeTodoInput!) {\n  makeTodo(makeTodoInput: $makeTodoInput) {\n    todo {\n      id\n      title\n      isCompleted\n      createdAt\n      updatedAt\n    }\n  }\n}"): (typeof documents)["mutation MakeTodo($makeTodoInput: MakeTodoInput!) {\n  makeTodo(makeTodoInput: $makeTodoInput) {\n    todo {\n      id\n      title\n      isCompleted\n      createdAt\n      updatedAt\n    }\n  }\n}"];
+export function graphql(source: "\n  mutation MakeTodo($makeTodoInput: MakeTodoInput!) {\n    makeTodo(makeTodoInput: $makeTodoInput) {\n      todo {\n        id\n        title\n        isCompleted\n        createdAt\n        updatedAt\n      }\n    }\n  }\n"): (typeof documents)["\n  mutation MakeTodo($makeTodoInput: MakeTodoInput!) {\n    makeTodo(makeTodoInput: $makeTodoInput) {\n      todo {\n        id\n        title\n        isCompleted\n        createdAt\n        updatedAt\n      }\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "mutation RemoveTodo($removeTodoInput: RemoveTodoInput!) {\n  removeTodo(removeTodoInput: $removeTodoInput) {\n    todo {\n      id\n      title\n    }\n  }\n}"): (typeof documents)["mutation RemoveTodo($removeTodoInput: RemoveTodoInput!) {\n  removeTodo(removeTodoInput: $removeTodoInput) {\n    todo {\n      id\n      title\n    }\n  }\n}"];
+export function graphql(source: "\n  mutation RemoveTodo($removeTodoInput: RemoveTodoInput!) {\n    removeTodo(removeTodoInput: $removeTodoInput) {\n      todo {\n        id\n        title\n      }\n    }\n  }\n"): (typeof documents)["\n  mutation RemoveTodo($removeTodoInput: RemoveTodoInput!) {\n    removeTodo(removeTodoInput: $removeTodoInput) {\n      todo {\n        id\n        title\n      }\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "mutation UpdateTodo($updateTodoInput: UpdateTodoInput!) {\n  updateTodo(updateTodoInput: $updateTodoInput) {\n    todo {\n      id\n      title\n      isCompleted\n      updatedAt\n      createdAt\n    }\n  }\n}"): (typeof documents)["mutation UpdateTodo($updateTodoInput: UpdateTodoInput!) {\n  updateTodo(updateTodoInput: $updateTodoInput) {\n    todo {\n      id\n      title\n      isCompleted\n      updatedAt\n      createdAt\n    }\n  }\n}"];
+export function graphql(source: "\n  mutation UpdateTodo($updateTodoInput: UpdateTodoInput!) {\n    updateTodo(updateTodoInput: $updateTodoInput) {\n      todo {\n        id\n        title\n        isCompleted\n        updatedAt\n        createdAt\n      }\n    }\n  }\n"): (typeof documents)["\n  mutation UpdateTodo($updateTodoInput: UpdateTodoInput!) {\n    updateTodo(updateTodoInput: $updateTodoInput) {\n      todo {\n        id\n        title\n        isCompleted\n        updatedAt\n        createdAt\n      }\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "query Todos {\n  getTodos {\n    todos {\n      id\n      title\n      isCompleted\n      createdAt\n      updatedAt\n    }\n  }\n}"): (typeof documents)["query Todos {\n  getTodos {\n    todos {\n      id\n      title\n      isCompleted\n      createdAt\n      updatedAt\n    }\n  }\n}"];
+export function graphql(source: "\n  query Todos {\n    getTodos {\n      todos {\n        id\n        title\n        isCompleted\n        createdAt\n        updatedAt\n      }\n    }\n  }\n"): (typeof documents)["\n  query Todos {\n    getTodos {\n      todos {\n        id\n        title\n        isCompleted\n        createdAt\n        updatedAt\n      }\n    }\n  }\n"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
