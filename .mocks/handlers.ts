088373bf217dbb0efaad4b3be5e7b4ef97dd56d1
seiha-tsuge <seiha.tsuge@gmail.com>
2023-06-13 18:19:38 +0900

    feat: handlersを作成

--- .mocks/handlers.ts
+++ .mocks/handlers.ts
@@ -1,45 +1,109 @@
-// src/mocks/handlers.js
 import { graphql } from "msw";
+import { factory, primaryKey } from "@mswjs/data";
+import type {
+  TodosQuery,
+  MakeTodoMutation,
+  MakeTodoMutationVariables,
+  RemoveTodoMutation,
+  RemoveTodoMutationVariables,
+  UpdateTodoMutation,
+  UpdateTodoMutationVariables,
+} from "../src/generated/gql/graphql";
+
+const db = factory({
+  todo: {
+    id: primaryKey(String),
+    title: String,
+  },
+});
+
+const todos = db.todo.toHandlers("graphql");
 
 export const handlers = [
-  // Handles a "Login" mutation
-  graphql.mutation("Login", (req, res, ctx) => {
-    const { username } = req.variables;
-    sessionStorage.setItem("is-authenticated", username);
-
+  graphql.query<TodosQuery>("Todos", (req, res, ctx) => {
     return res(
       ctx.data({
-        login: {
-          username,
+        getTodos: {
+          todos: [
+            {
+              id: "1",
+              title: "Todo 1",
+              isCompleted: false,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
+            {
+              id: "2",
+              title: "Todo 2",
+              isCompleted: false,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
+            {
+              id: "3",
+              title: "Todo 3",
+              isCompleted: true,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
+          ],
         },
       })
     );
   }),
 
-  // Handles a "GetUserInfo" query
-  graphql.query("GetUserInfo", (req, res, ctx) => {
-    const authenticatedUser = sessionStorage.getItem("is-authenticated");
-
-    if (!authenticatedUser) {
-      // When not authenticated, respond with an error
+  graphql.mutation<MakeTodoMutation, MakeTodoMutationVariables>(
+    "MakeTodo",
+    (req, res, ctx) => {
       return res(
-        ctx.errors([
-          {
-            message: "Not authenticated",
-            errorType: "AuthenticationError",
+        ctx.data({
+          makeTodo: {
+            todo: {
+              id: "ac56032b-dc31-4937-90a3-444960d793c8",
+              title: req.variables.makeTodoInput.title,
+              isCompleted: false,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
           },
-        ])
+        })
       );
     }
+  ),
 
-    // When authenticated, respond with a query payload
-    return res(
-      ctx.data({
-        user: {
-          username: authenticatedUser,
-          firstName: "John",
-        },
-      })
-    );
-  }),
+  graphql.mutation<RemoveTodoMutation, RemoveTodoMutationVariables>(
+    "RemoveTodo",
+    (req, res, ctx) => {
+      return res(
+        ctx.data({
+          removeTodo: {
+            todo: {
+              id: req.variables.removeTodoInput.todoId,
+              title: "Todo 1",
+            },
+          },
+        })
+      );
+    }
+  ),
+
+  graphql.mutation<UpdateTodoMutation, UpdateTodoMutationVariables>(
+    "UpdateTodo",
+    (req, res, ctx) => {
+      const { isCompleted, title } = req.variables.updateTodoInput;
+      return res(
+        ctx.data({
+          updateTodo: {
+            todo: {
+              id: "ac56032b-dc31-4937-90a3-444960d793c8",
+              title: title ? title : "Todo 1",
+              isCompleted: isCompleted ? isCompleted : false,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
+          },
+        })
+      );
+    }
+  ),
 ];
