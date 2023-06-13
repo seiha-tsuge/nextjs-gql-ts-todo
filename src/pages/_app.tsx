449e38d069107b7ef63d37db7154807624f62caa
seiha-tsuge <seiha.tsuge@gmail.com>
2023-06-13 14:39:28 +0900

    feat: msw set up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import { AppProvider } from "@/providers/app";
 
 import type { AppPropsWithLayout } from "@/types/next";
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+  require("../../.mocks");
+}
+
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
   return <AppProvider>{getLayout(<Component {...pageProps} />)}</AppProvider>;
